e6fb321ca67edc4b18b1c6587d59ae269ba6d3cb
hercozauzau <zauzauherco@gmail.com>
2024-04-16 12:32:39 +0200

    update fakeapi

---
+++ src/fakeapi.js
@@ -0,0 +1,471 @@
+export const melhoresDestinos = [
+  {
+    id: 1,
+    nome: 'Cidade de Maputo',
+    img: 'https://media.istockphoto.com/id/1340599897/photo/night-city.jpg?s=612x612&w=0&k=20&c=Qu2yjFR2hKbK3bbDhe9LDeg9NvUXdJ0z3vmurYqkb-I=',
+    nota: 2,
+    desc: 'Maputo, a vibrante capital de Moçambique, combina cultura, história e modernidade. Mercado Municipal, Avenida Marginal e culinária moçambicana são imperdíveis.',
+    fotos: [
+      'assets/destinos/destino(1).jpg',
+      'assets/destinos/destino(2).jpg',
+      'assets/destinos/destino(3).jpg',
+      'assets/destinos/destino(4).jpg',
+      'assets/destinos/destino(5).jpg',
+      'assets/destinos/destino(6).jpg',
+      'assets/destinos/destino(7).jpg',
+      'assets/destinos/destino(8).jpg',
+      'assets/destinos/destino(9).jpg',
+      'assets/destinos/destino(10).jpg',
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Parque de Gorongosa',
+    img: 'https://media.istockphoto.com/id/831606046/photo/inselbergs-of-northern-mozambique.jpg?s=612x612&w=0&k=20&c=rjO7PGHomuSTGCv_2cTygSDZnMgmzOILGnWcOJxH24w=',
+    nota: 3,
+    desc: 'O Parque Nacional da Gorongosa destaca-se pela biodiversidade incrível. Safáris emocionantes oferecem avistamentos de elefantes, leões e uma variedade de aves. Projetos de conservação e comunidade enriquecem a experiência.',
+  },
+  {
+    id: 3,
+    nome: 'Katembe',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Tz9GCTdNlQVCm6Usyd4bsi3KqVWofVd96xEcRuY_JWduBrgViJOQqkhuNd-Cq5ujG8o&usqp=CAU',
+    nota: 3,
+    desc: 'Katembe, do outro lado da baía de Maputo, oferece vistas panorâmicas deslumbrantes. A Ponte Maputo-Katembe leva a praias serenas e atmosfera tranquila, perfeitas para relaxar.',
+  },
+  {
+    id: 4,
+    nome: 'Praia de Bilene',
+    img: 'https://www.mozambiquetourism.co.za/wp-content/uploads/2020/04/Anantara-Medjumbe-Medium-1488-scaled.jpg',
+    nota: 2,
+    desc: 'Bilene é um refúgio costeiro com águas serenas e areias brancas. Ideal para desportos aquáticos, a Lagoa Uembje atrai amantes do caiaque e da vela. Um oásis tranquilo à beira da água.',
+  },
+  {
+    id: 5,
+    nome: 'Ponta do Ouro',
+    img: 'https://media.istockphoto.com/id/1222450709/photo/ponta-do-ouro-pristine-beach-and-town-in-mozambique-coastline-near-border-of-south-africa.jpg?s=612x612&w=0&k=20&c=6Cvn2TDvO_jPc_ghN5bqcai5W0ieqVyjlqtBIZ0T6ec=',
+    nota: 4,
+    desc: 'No extremo sul de Moçambique, Ponta do Ouro é conhecida por suas águas cristalinas e mergulho excepcional. Próxima à Reserva Marinha Parcial, oferece avistamentos de golfinhos e tartarugas.',
+  },
+  {
+    id: 7,
+    nome: 'Corais de Tofo',
+    img: 'https://media.istockphoto.com/id/498283106/photo/underwater-scuba-diver-explore-and-enjoy-coral-reef-sea-life.jpg?s=612x612&w=0&k=20&c=xOj00xaZTpy5-AtKvMvIHHfexz9miSSct_CXb6F9KVA=',
+    nota: 1,
+    desc: 'Ao largo da Baía de Inhambane, os Corais de Tofo são uma maravilha subaquática. Recifes de coral impressionantes e mergulho com tubarões-baleia atraem entusiastas. Atmosfera descontraída em Tofo.',
+  },
+  {
+    id: 8,
+    nome: 'Ilha de Moçambique',
+    img: 'https://media.istockphoto.com/id/1061009910/photo/fort-s%C3%A3o-sebasti%C3%A3o-mozambique-island-indian-ocean-coast-mo%C3%A7ambique-mozambique-channel.jpg?s=612x612&w=0&k=20&c=o7IEf56lIR6J4NqYItNl9ckotWhzmiWqn7kOEtKwrDY=',
+    nota: 3,
+    desc: 'A Ilha de Moçambique, Patrimônio Mundial da UNESCO, é um tesouro histórico. Arquitetura influenciada por árabes, portugueses e africanos conta uma história rica. Praias tranquilas completam a experiência única.',
+  },
+];
+
+export const melhoresAtracoes = {
+  lista: [
+    {
+      nome: 'Mariscos',
+      provincia: 'Inhambane',
+      descricao:
+        'Em Moçambique, os mariscos são uma atração gastronômica imperdível, com uma abundância de frutos do mar frescos e deliciosos, que podem ser apreciados em restaurantes à beira-mar.',
+      img: 'https://media.istockphoto.com/id/1436900776/photo/fresh-seafood-shrimps-served-on-table-with-wine.jpg?s=612x612&w=0&k=20&c=gs04qg8DusExXoWBIceuUVSYkUQPTzNVxLcV84_Nrho=',
+      nota: 2,
+      tipo: 1,
+    },
+    {
+      nome: 'Casas de vinho',
+      provincia: 'Maputo Cidade',
+      descricao:
+        'As casas de vinho em Moçambique oferecem uma experiência única para os amantes de vinho, onde se pode degustar vinhos locais e internacionais em um ambiente aconchegante e charmoso.',
+      img: 'https://media.istockphoto.com/id/1437988182/photo/bartender-pouring-red-wine-from-bottle-into-glass-indoors-closeup.jpg?s=612x612&w=0&k=20&c=eT-I8Zg2w5JkTq9ZeqkKkgoCp0IUTLAGQWTQmda6SoY=',
+      nota: 5,
+      tipo: 1,
+    },
+    {
+      nome: 'Pratos Tradicionais',
+      provincia: 'Nampula',
+      descricao:
+        'A culinária moçambicana é uma verdadeira celebração de sabores e tradições locais, com pratos como o peri-peri, caril e matapa, que proporcionam uma experiência gastronômica autêntica.',
+      img: 'https://media.istockphoto.com/id/1143191120/photo/traditional-moroccan-tajine-of-chicken-with-dried-fruits-and-spices.jpg?s=612x612&w=0&k=20&c=KTNBlSpeS18SGMNacLGxPbVh0MRRnlPrEKL1jJAZ-es=',
+      nota: 5,
+      tipo: 1,
+    },
+    {
+      nome: 'Baía dos Golfinhos',
+      provincia: 'Inhambane',
+      descricao:
+        'A Baía dos Golfinhos é um refúgio natural espetacular em Moçambique, onde é possível avistar golfinhos em seu ambiente natural, desfrutar de praias de areia branca e explorar recifes de coral incríveis.',
+      img: 'https://media.istockphoto.com/id/1303043820/photo/mother-and-calf-dolphin-swimming-by.jpg?s=612x612&w=0&k=20&c=Ltiw99iBFTnWCs9MOqzRKiNEfz3GgDQOz6A6FYeVuD0=',
+      nota: 0,
+      tipo: 2,
+    },
+    {
+      nome: 'Rio Zambezi',
+      provincia: 'Zambézia',
+      descricao:
+        'O majestoso Rio Zambezi serpenteia por Moçambique, oferecendo oportunidades para safáris aquáticos, pesca e aventuras nas proximidades das maravilhosas e imperdiveis Cataratas Vitória.',
+      img: 'https://media.istockphoto.com/id/1347088244/photo/kerala-most-beautiful-place-of-india.jpg?s=612x612&w=0&k=20&c=oBVJ6rUfq1YOua_4Oqhj0B1TFwcMFhniaysLJEN_eh8=',
+      nota: 0,
+      tipo: 2,
+    },
+    {
+      nome: 'Zoológicos',
+      provincia: 'Sofala',
+      descricao:
+        'Moçambique possui zoológicos que abrigam uma variedade de animais selvagens, proporcionando aos visitantes a oportunidade de aprender sobre a fauna local e global em primeira.',
+      img: 'https://media.istockphoto.com/id/173681964/photo/zoo.jpg?s=612x612&w=0&k=20&c=bwZzd8L9OcF9Qq7KqrsGyr7GZ69TIAXlnrTNi4MTDis=',
+      nota: 0,
+      tipo: 2,
+    },
+    {
+      nome: 'Museu da História Natural',
+      provincia: 'Maputo Cidade',
+      descricao:
+        'O Museu da História Natural em Moçambique abriga uma rica e diversa coleção de fósseis, minerais e artefatos culturais, oferecendo uma bela visão fascinante da história natural do país.',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV-rE0PgS6HXHTJbwqnm9q0NlEE03TVBRi1A&usqp=CAU',
+      nota: 4,
+      tipo: 3,
+    },
+    {
+      nome: 'Fortaleza de Maputo',
+      provincia: 'Maputo CIdade',
+      descricao:
+        'A Fortaleza de Maputo é um marco histórico que remonta à era colonial, com sua arquitetura, oferecendo uma bela visão sobre a história e a cultura moçambicana em todos pontos nacionais.',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd7auFSsk-GKiz1-ClZS91V7a2KyDyv15-HA&usqp=CAU',
+      nota: 2,
+      tipo: 3,
+    },
+    {
+      nome: 'Casa de Ferro',
+      provincia: 'Maputo Cidade',
+      descricao:
+        ' A Casa de Ferro, projetada por Gustave Eiffel decadas passadas, é uma estrutura icônica em Moçambique, conhecida por sua arquitetura única, bela, histórica e cultural.',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwdZv_PYRSloW0Q_Ynl3ZNCFDWQ-5e9Z6l1w&usqp=CAU',
+      nota: 4,
+      tipo: 3,
+    },
+    {
+      nome: 'Cachoeiras de Bazruto',
+      provincia: 'Inhambane',
+      descricao:
+        ' As Cachoeiras de Bazruto são um paraíso natural aqui escondido, onde os visitantes podem relaxar e desfrutar de quedas de água deslumbrantes em meio à exuberante vegetação da natureza.',
+      img: 'https://media.istockphoto.com/id/1412792961/photo/woman-hiking-in-a-lush-green-forest-near-a-flowing-river.jpg?s=612x612&w=0&k=20&c=u1P7fipF4OVyeABdSGRnY9tBwvoDi0TbP_nuMcQOrwM=',
+      nota: 5,
+      tipo: 4,
+    },
+    {
+      nome: 'Corais de Bilene',
+      provincia: 'Gaza',
+      descricao:
+        'Os corais de Bilene são ideais para mergulho e snorkeling, onde se pode explorar a rica vida marinha e os vibrantes recifes de coral que caracterizam as águas costeiras de Moçambique.',
+      img: 'https://media.istockphoto.com/id/1421365707/photo/scuba-divers-couple-near-beautiful-coral-reef-watching-sea-anemone-and-family-of-two-banded.jpg?s=612x612&w=0&k=20&c=3ixYAwkxJOo451J2ht3ocZsolpf4W4MEA0CcJ_mi8rc=',
+      nota: 1,
+      tipo: 4,
+    },
+    {
+      nome: 'Parque de Gorongosa',
+      provincia: 'Sofala',
+      descricao:
+        'O Parque Nacional da Gorongosa, em Moçambique, é um santuário de biodiversidade que abriga uma variada fauna e flora, incluindo elefantes, girafas, hienas, leões e aves raras.',
+      img: 'https://media.istockphoto.com/id/623057548/photo/elephants-from-maasai-mara.jpg?s=612x612&w=0&k=20&c=I1_ACyG542MYvqnoaKr07Tb9XId9mCqpKO267iyVltg=',
+      nota: 3,
+      tipo: 4,
+    },
+  ],
+};
+
+export const topGuias = [
+  {
+    id: 1,
+    nome: 'Marcos Chissano',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Matola',
+    nota: 3,
+    Fotos: [
+      {
+        url: 'assets/guias/guia(1).jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Carlos Monteiro',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Niassa',
+    nota: 3,
+    Fotos: [
+      {
+        url: 'assets/guias/guia(2).jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 3,
+    nome: 'Adelaide Matsinhe',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Gaza',
+    nota: 3,
+    Fotos: [
+      {
+        url: 'assets/guias/guia(3).jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 4,
+    nome: 'Victor Dias',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 3,
+    Fotos: [
+      {
+        url: 'assets/guias/guia(4).jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+];
+
+export const listaDeGuias = [
+  {
+    id: 1,
+    nome: 'Jhonny Sins',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Niassa',
+    nota: 3,
+    Fotos: [
+      {
+        url: 'https://i0.wp.com/www.socialnews.xyz/wp-content/uploads/2019/09/04/3b9764c1ff26a3f5c74896adf521f911.jpg?quality=80&zoom=1&ssl=1',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+  {
+    id: 2,
+    nome: 'Terry Crews',
+    sobrenome: 'zauzau',
+    email: 'Guia certificado',
+    local: 'Maputo',
+    nota: 5,
+    Fotos: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Terry_Crews_by_Gage_Skidmore_5.jpg/330px-Terry_Crews_by_Gage_Skidmore_5.jpg',
+        filename: 'foto',
+      },
+    ],
+  },
+];
+
+export const galeria = [
+  {
+    id: 1,
+    url: 'https://images.unsplash.com/photo-1486655643111-5a1741acd481?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1029&q=80',
+    tipo: 1,
+  },
+  {
+    id: 2,
+    url: 'https://media.istockphoto.com/id/1201669316/photo/happy-girl-climber-on-break.jpg?s=612x612&w=0&k=20&c=npY9NPK8NAiXfFCadz4N8laGryYBHminNyFWUHkNPlc=',
+    tipo: 1,
+  },
+  {
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1607130661410-22d1f9535687?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGJsYWNrJTIwdHJhdmVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    tipo: 2,
+  },
+  {
+    id: 4,
+    url: 'https://media.istockphoto.com/id/1031430214/photo/young-woman-kayaking-through-the-backwaters-of-monroe-island.jpg?s=612x612&w=0&k=20&c=kbv2s1kknMzJgk8Nd-W2VNIf0AFx48YtCqygtI3Ppos=',
+    tipo: 3,
+  },
+  {
+    id: 5,
+    url: 'https://media.istockphoto.com/id/598177426/photo/indonesian.jpg?s=612x612&w=0&k=20&c=qCbDG1de40x7_7Dujjb-m15UKt0ONzNbqNybRqV2EXA=',
+    tipo: 1,
+  },
+  {
+    id: 6,
+    url: 'https://media.istockphoto.com/id/468053398/photo/boabab-alley-sunset.jpg?s=612x612&w=0&k=20&c=1zB8WsmWo9Gi-zDHKusJFStE7EWZ6tFrekfPv5N13Tk=',
+    tipo: 3,
+  },
+  {
+    id: 7,
+    url: 'https://media.istockphoto.com/id/1222770098/photo/aerial-view-of-bicycle-shadows-on-the-empty-asphalt-road-between-rapeseed-field.jpg?s=612x612&w=0&k=20&c=Jq0nqpKGJXGviXk5LBxLUaG1MPonWWwUyTfWFyLPjzA=',
+    tipo: 3,
+  },
+  {
+    id: 8,
+    url: 'https://media.istockphoto.com/id/1334099583/photo/looking-for-our-next-sightseeing-spot.jpg?s=612x612&w=0&k=20&c=VPvXS6NrcHdINQAv2N3iTKdvj9GgaVHcrjRkISfzVOw=',
+    tipo: 3,
+  },
+  {
+    id: 9,
+    url: 'https://theworldpursuit.com/wp-content/uploads/2016/08/Mozambique-Travel33-1024x683.jpg',
+    tipo: 3,
+  },
+  {
+    id: 10,
+    url: 'https://static.lpnt.fr/images/2015/12/08/2603588lpaw-2603659-article-jpg_3201196_660x287.jpg',
+    tipo: 2,
+  },
+  {
+    id: 11,
+    url: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/0c/10/4f/bd.jpg',
+    tipo: 2,
+  },
+  {
+    id: 12,
+    url: 'https://ivhq.imgix.net/images/map/ivhq-volunteer-abroad-region-africa-hero.jpg?w=945&h=700&fit=crop&crop=center&q=85&auto=format,compress',
+    tipo: 2,
+  },
+  {
+    id: 13,
+    url: 'https://cdn01.buxtonco.com/news/2661/istock-665028882__large.jpg',
+    tipo: 1,
+  },
+  {
+    id: 14,
+    url: 'https://static.wixstatic.com/media/e75c5e_2c6f3122e58942e084abb686da35a3e1~mv2.jpg/v1/fill/w_1899,h_725,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e75c5e_2c6f3122e58942e084abb686da35a3e1~mv2.jpg',
+    tipo: 3,
+  },
+  {
+    id: 15,
+    url: 'https://www.sunnysports.com/blog/wp-content/uploads/2021/11/tommy-lisbin-2DH-qMX6M4E-unsplash.jpg',
+    tipo: 1,
+  },
+  {
+    id: 16,
+    url: 'https://images.unsplash.com/photo-1565158251535-ad86fe2a2dcd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    tipo: 2,
+  },
+  {
+    id: 17,
+    url: 'https://plus.unsplash.com/premium_photo-1676982098817-844e52754258?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHRvdXJpc218ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    tipo: 3,
+  },
+  {
+    id: 18,
+    url: 'https://images.unsplash.com/photo-1563693267403-111c5d856e49?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHRvdXJpc218ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    tipo: 3,
+  },
+];
